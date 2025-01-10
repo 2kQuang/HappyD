a2d8e7cb31fc00a8427916c8d9cf00afb46256d9
2kQuang <tvquang.19i@cit.udn.vn>
2025-01-10 19:10:45 +0700

    Done

---
+++ data.js
@@ -0,0 +1,8 @@
+const data_name = [
+    {
+        ma_ten: "Quang",
+        name: "Quang",
+        message: "Hello, I'm Quang",
+        img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
+    }
+]
